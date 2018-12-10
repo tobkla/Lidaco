761b23a1b26b582731265d420170b46529a8fc13
Luaks Pauscher <lukas.pauscher@iee.fraunhofer.de>
2018-12-10 22:18:06 +0100

    added for loop to run for several measurement ids simultaniously.

--- run_in_spyder.py
+++ run_in_spyder.py
@@ -1,25 +1,21 @@
 from lidaco.core.Builder import Builder
 import os
 
-meas_ID = '88'
 
-
-errLog = r'.\Lidar data\converted\0'+meas_ID+'\error.log'
-if os.path.isfile(errLog):
-    os.remove(errLog)
-
-# user must set the configuration file for the specific data set
-file1 = r'C:\Users\lpauscher\Documents\python_scripts\git\Lidaco\yamls\meas_IDs\Meas_ID_'+meas_ID+'.yaml'
-
-builder = Builder(config_file = file1)
-builder.build()
-
-
-errLog = r'.\Lidar data\converted\0'+meas_ID+'\error.log'
-
-if os.path.isfile(errLog):
-    file = open(errLog, 'r') 
-    errFiles = file.readlines()
-    
-    for file in errFiles:   
-        os.remove(file[:-1])
+meas_IDs = list(range(84, 86))
+for meas_ID in meas_IDs:
+    meas_ID = str(meas_ID)
+    errLog = r'.\Lidar data\converted\0'+meas_ID+'\error.log'
+    if os.path.isfile(errLog):
+        os.remove(errLog)    
+    # user must set the configuration file for the specific data set
+    file1 = r'C:\Users\lpauscher\Documents\python_scripts\git\Lidaco\yamls\meas_IDs\Meas_ID_'+meas_ID+'.yaml'
+    builder = Builder(config_file = file1)
+    builder.build()
+    errLog = r'.\Lidar data\converted\0'+meas_ID+'\error.log'
+    if os.path.isfile(errLog):
+        file = open(errLog, 'r') 
+        errFiles = file.readlines()
+        
+        for file in errFiles:   
+            os.remove(file[:-1])
